2111aeb8152f87856afdfe99a5ae748e94b02d87
Fagner La Selva <fagnerlaselva@gmail.com>
2024-05-06 18:33:42 -0300

    add: buttom edit article

--- src/components/app/card/Article.vue
+++ src/components/app/card/Article.vue
@@ -1,8 +1,8 @@
 <template>
-
-  <article class="d-flex flex-sm-column flex-lg-row px-3 border-bottom  align-items-center" data-bs-theme="auto">
+  <article class="d-flex flex-sm-column flex-lg-row px-3 border-bottom align-items-center justify-content-between"
+    data-bs-theme="auto">
     <div class=" d-flex justify-content-between">
-      <RouterLink :to="{ name: 'ArticleEditor' }">
+      <div>
         <div class="flex">
           <div class="d-inline-flex align-items-center">
             <img src="https://avatars.githubusercontent.com/u/34191081?v=4" width="32" height="32"
@@ -16,31 +16,36 @@
         <div class="mt-2">Mailing o que é e como utilizá-lo de maneira inteligente.</div>
         <div class="description-article">Mailing é um termo que tem origem do vocabulário inglês que é o
           ato de enviar uma correspondência ...</div>
-        <div class="d-inline-flex category-article py-2">
+        <div class="d-inline-flex category-article py-2 align-items-center">
+          <div>
+            <RouterLink :to="{ name: 'ArticleEditor' }" class="view-article ">
+              Editar
+            </RouterLink>
+          </div>
           <div class="">
-            <div class="view-article pr-2">400 visualizações</div>
+            <div class="view-article px-2">400 visualizações</div>
           </div>
           <div class="">
             <div class="view-article px-2">#Mailing</div>
           </div>
         </div>
-
-      </RouterLink>
+      </div>
     </div>
-    <div class="flex d-none d-xl-block">
+    <div class="flex d-none d-xl-block ">
       <img src="../../../../public/images/image-article.png" width="238" height="139" class="img-article-card">
     </div>
   </article>
 
-  <article class="d-flex flex-sm-column flex-lg-row px-3 border-bottom align-items-center" data-bs-theme="auto">
-    <RouterLink :to="{ name: 'ArticleEditor' }">
+  <article
+    class="d-flex flex-sm-column flex-lg-row px-3 border-bottom justice align-items-center justify-content-between"
+    data-bs-theme="auto">
+    <div>
       <div class="flex">
         <div class="d-inline-flex align-items-center">
           <img src="https://avatars.githubusercontent.com/u/17029609?v=4" width="32" height="32" class="rounded-circle">
           <div class="d-flex flex-column flex-sm-row">
             <div class="date-article px-2">Postado em 05 maio 2023</div>
             <div class="name-author px-2">kratinho Lima</div>
-
           </div>
         </div>
       </div>
@@ -48,15 +53,20 @@
       <div class="description-article">Mailing é um termo que tem origem do vocabulário inglês que é o
         ato de enviar uma correspondência ...
       </div>
-      <div class="d-inline-flex category-article py-2">
+      <div class="d-inline-flex category-article py-2 align-items-center">
+        <div>
+          <RouterLink :to="{ name: 'ArticleEditor' }" class="view-article ">
+            Editar
+          </RouterLink>
+        </div>
         <div class="">
-          <div class="view-article pr-2">400 visualizações</div>
+          <div class="view-article px-2">400 visualizações</div>
         </div>
         <div class="">
           <div class="view-article px-2">#Mailing</div>
         </div>
       </div>
-    </RouterLink>
+    </div>
     <div class="flex d-none d-xl-block ">
       <img src="../../../../public/images/image-article-2.png" width="238" height="139" class="img-article-card">
     </div>
@@ -80,9 +90,14 @@ article:hover {
   background: rgba(var(--bs-tertiary-bg-rgb), var(--bs-bg-opacity));
 }
 
+
 article a {
+  color: var(--bs-primary);
+  font-size: 13px;
   text-decoration: none;
 }
+
+article a:hover {}
 
 
 .name-author {
