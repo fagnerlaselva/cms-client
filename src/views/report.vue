7d2e4dc685bd5ab2fbb8c34227d08beaf55cca2b
Fagner La Selva <fagnerlaselva@gmail.com>
2024-05-14 11:41:52 -0300

    marquee

--- src/views/report.vue
+++ src/views/report.vue
@@ -46,10 +46,90 @@
         </div>
       </div>
     </div>
+
+    <div class="marquee">
+      <div>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+        <span>Em breve novidades</span>
+      </div>
+    </div>
   </section>
 
 </template>
 <style>
+.marquee {
+  height: 35px;
+  width: 100%;
+  margin-left: -28px;
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  padding: 8px 0 4px 0;
+}
+
+.marquee div {
+  display: inline-block;
+  width: 300%;
+  height: 40px;
+  position: absolute;
+  overflow: hidden;
+  animation: marquee 30s linear infinite;
+}
+
+.marquee span {
+  float: left;
+  width: 200px;
+}
+
+.marquee:hover div {
+  animation-play-state: paused;
+}
+
+@keyframes marquee {
+  0% {
+    left: 0%;
+  }
+
+  100% {
+    left: -1100px;
+  }
+}
+
+/* @import must be at top of file, otherwise CSS will not work */
+@import url("//hello.myfonts.net/count/3909a7");
+
+@font-face {
+  font-family: 'UniversLTPro-Ex';
+  src: url('webfonts/3909A7_0_0.eot');
+  src: url('webfonts/3909A7_0_0.eot?#iefix') format('embedded-opentype'), url('webfonts/3909A7_0_0.woff2') format('woff2'), url('webfonts/3909A7_0_0.woff') format('woff'), url('webfonts/3909A7_0_0.ttf') format('truetype');
+}
+
 .card-title {
   font-weight: 600;
 }
