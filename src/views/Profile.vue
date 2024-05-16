ef1f6b2ea0e8f7bfe55f6a76b130957eea1b6388
Fagner La Selva <fagnerlaselva@gmail.com>
2024-05-16 18:38:21 -0300

    add: add-link and remove-link

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -23,19 +23,7 @@
           :inputClass="'custom-file-input'" :name="'profile-image'" :accept="'image/png, image/jpeg, image/webp'" />
 
         <ImageUploader labelText="Selecione uma imagem" inputId="image-perfil" />
-        <!--
-        <div class="row my-3 py-4">
-          <label for="colFormLabelName" class="col-sm-3 col-form-label">Adicionar imagem:</label>
-          <div class="col-sm-8">
-            <div class="thubnail-member rounded position-relative">
-              <div class="img-membro-upload border bg-light-subtle  rounded-circle float-start position-absolute">
-                <img src="https://avatars.githubusercontent.com/u/34191081?v=4" class="rounded-circle"
-                  alt="Fagner Mendes" height="155" width="155">
-              </div>
-            </div>
-          </div>
-        </div>
--->
+
         <div class="row my-3">
           <label for="colFormLabelName" class="col-sm-3 col-form-label text-md-end">Nome:</label>
           <div class="col-sm-8">
@@ -62,7 +50,6 @@
             <div class="">
               Biografia do Autor:
             </div>
-
           </label>
           <div class="col-sm-8">
             <textarea v-model="message" class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
@@ -71,58 +58,65 @@
           <p></p>
         </div>
 
-
-        <div class="p-4 bg-body-tertiary rounded-2">
-          <div class="row my-3">
-            <div class="col-sm-3 col-form-label"></div>
-            <div class="col-sm-8">
-              <i>
-                Links para as contas de redes sociais, como Twitter, Facebook, LinkedIn, entre outros, são
-                fundamentais neste espaço. Sinta-se à vontade para adicionar quantos forem relevantes para sua
-                presença online.
-              </i>
-            </div>
-          </div>
-          <div class="row my-3">
-            <label for="colFormLabelLink" class="col-sm-3 col-form-label text-md-end"> Link:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="colFormLabelLink" placeholder="">
-            </div>
-          </div>
-          <div class="row my-3">
-            <label for="colFormLabelName" class="col-sm-3 col-form-label text-md-end">Nome do link:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="colFormLabelLink" placeholder="">
-            </div>
-            <div class="col-sm-1">
-              <button type="button" class="btn buttom-action-add d-flex">
-                <div class="rounded-circle border bg-primário bg-gradiente d-inline-flex p-2">
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 6L11 6" stroke="#FF4655" stroke-width="2" stroke-linecap="round" />
-                  </svg>
+        <div>
+          <div v-for="(block, index) in blocks" :key="block.id" class="add-new-block-links" :id="'block-' + block.id">
+            <div class="mt-4 p-4 bg-body-tertiary rounded-2">
+              <!-- Conteúdo do bloco -->
+              <div class="row my-3">
+                <div class="col-sm-3 col-form-label"></div>
+                <div class="col-sm-8">
+                  <i>
+                    Links para as contas de redes sociais, como Twitter, Facebook, LinkedIn, entre outros, são
+                    fundamentais neste espaço. Sinta-se à vontade para adicionar quantos forem relevantes para sua
+                    presença online.
+                  </i>
                 </div>
-              </button>
+              </div>
+              <div class="row my-3">
+                <label for="colFormLabelLink" class="col-sm-3 col-form-label text-md-end"> Link:</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="colFormLabelLink" placeholder="" value="">
+                </div>
+              </div>
+              <div class="row my-3">
+                <label for="colFormLabelName" class="col-sm-3 col-form-label text-md-end">Nome do link:</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="colFormLabelLink" placeholder="" value="">
+                </div>
+                <div class="col-sm-1">
+                  <button type="button" class="btn buttom-action-add d-flex re" @click="removeBlock(index)">
+                    <div class="rounded-circle border bg-primário bg-gradiente d-inline-flex p-2 rem">
+                      <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 6L11 6" stroke="#FF4655" stroke-width="2" stroke-linecap="round" />
+                      </svg>
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row my-3">
+            <div class="col-sm-4">
+              <div class="py-4 d-flex justify-content-between">
+                <button type="button" class="btn buttom-action-add d-flex align-items-center add-link"
+                  @click="adicionarBloco">
+                  <div class="rounded-circle border bg-primário bg-gradiente d-inline-flex p-2">
+                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6 1V11" stroke="#35A936" stroke-width="2" stroke-linecap="round" />
+                      <path d="M1 6L11 6" stroke="#35A936" stroke-width="2" stroke-linecap="round" />
+                    </svg>
+                  </div>
+                  <div class="px-2">Adicionar mais um link</div>
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
 
-        <div class="row my-3">
-          <div class="col-sm-4">
-            <div class="py-4 d-flex justify-content-between  ">
-              <button type="button" class="btn buttom-action-add d-flex  align-items-center ">
-                <div class="rounded-circle border bg-primário bg-gradiente d-inline-flex p-2">
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 1V11" stroke="#35A936" stroke-width="2" stroke-linecap="round" />
-                    <path d="M1 6L11 6" stroke="#35A936" stroke-width="2" stroke-linecap="round" />
-                  </svg>
-                </div>
-                <div class="px-2">Adicionar mais um link</div>
-              </button>
-            </div>
-          </div>
-        </div>
-        <!-- Button trigger modal -->
+
+
+
         <div class="d-flex justify-content-end">
           <button type="button" class="btn btn-primary justify-content-end" data-bs-toggle="modal"
             data-bs-target="#staticBackdrop">
@@ -134,6 +128,47 @@
   </section>
 
 </template>
+
+<script>
+
+import ImageUploader from '@/components/generic/forms/UploadBanner.vue';
+import UploadPhotoPerfil from '@/components/generic/forms/UploadPhotoPerfil.vue';
+
+export default {
+  props: {
+
+  },
+
+  components: { ImageUploader, UploadPhotoPerfil },
+  name: 'ViewAddMember',
+
+  data() {
+    return {
+      message: 'Possuo 14 anos de experiência como Front-end developer, sou apaixonado por desenvolvimento web',
+      SeoPalavrasIdeial: 700,
+      blocks: [{ id: 1 }],
+    }
+  },
+  methods: {
+    adicionarBloco() {
+      const novoID = this.blocks.length > 0 ? this.blocks[this.blocks.length - 1].id + 1 : 1;
+      this.blocks.push({ id: novoID });
+    },
+    removeBlock(index) {
+      this.blocks.splice(index, 1); // Remove o bloco correspondente ao índice
+    }
+  },
+  computed: {
+    QuantideDePalavras() {
+      return this.message.length
+    },
+    SeoPalavrasFalta() {
+      return this.SeoPalavrasIdeial - this.QuantideDePalavras
+    }
+  }
+}
+</script>
+
 <style>
 .img-membro-upload {
   width: 155px;
@@ -152,32 +187,3 @@ form {
   font-weight: 300;
 }
 </style>
-<script>
-
-import ImageUploader from '@/components/generic/forms/UploadBanner.vue';
-import UploadPhotoPerfil from '@/components/generic/forms/UploadPhotoPerfil.vue';
-export default {
-  props: {
-
-  },
-
-  components: { ImageUploader, UploadPhotoPerfil },
-  name: 'ViewAddMember',
-
-  data() {
-    return {
-      message: 'Possuo 14 anos de experiência como Front-end developer, sou apaixonado por desenvolvimento web',
-      SeoPalavrasIdeial: 700,
-    }
-  },
-
-  computed: {
-    QuantideDePalavras() {
-      return this.message.length
-    },
-    SeoPalavrasFalta() {
-      return this.SeoPalavrasIdeial - this.QuantideDePalavras
-    }
-  }
-}
-</script>
