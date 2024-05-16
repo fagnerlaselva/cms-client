ef8710fd9ce297ad2127c7e581b21ae93b0fb4b7
Fagner La Selva <fagnerlaselva@gmail.com>
2024-05-16 17:31:00 -0300

    componete member

--- src/components/app/card/Member.vue
+++ src/components/app/card/Member.vue
@@ -1,37 +1,59 @@
 <template>
-  <article class="d-flex flex-row border-bottom px-3">
-    <div>
-      <div class="flex  align-items-center">
-        <div class="d-flex flex-row align-items-center">
-          <img src="https://avatars.githubusercontent.com/u/34191081?v=4" width="50" height="50" class="rounded-circle">
-          <div class="name-member p-2">Fagner Mendes</div>
-          <div><span class="badge text-bg-secondary">Admin</span></div>
+  <div>
+    <article v-for="(member, index) in members" :key="index" class="d-flex flex-row border-bottom px-3">
+      <div>
+        <div class="flex align-items-center">
+          <div class="d-flex flex-row align-items-center">
+            <img :src="member.image" width="50" height="50" class="rounded-circle">
+            <div class="name-member p-2">{{ member.name }}</div>
+            <div><span class="badge text-bg-secondary">{{ member.role }}</span></div>
+          </div>
+        </div>
+        <div class="description-member">{{ member.description }}</div>
+        <div class="d-inline-flex category-article py-2 align-items-center">
+          <div class="view-article">Escreveu <strong>{{ member.articles }}</strong> artigos e teve <strong>{{
+      member.views }}</strong> visualizações</div>
         </div>
       </div>
-      <div class="description-member ">Possuo 12 anos de experiência na área, sou criativo e apaixonado por
-        desenvolvimento web, possuo facilidade para trabalhar em equipe</div>
-      <div class="d-inline-flex category-article py-2  align-items-center">
-        <div class="view-article">Escreveu <strong>8</strong> artigos e teve <strong>3300</strong> visualizações</div>
-      </div>
-    </div>
-  </article>
-  <article class="d-flex flex-row border-bottom px-3">
-    <div>
-      <div class="flex  align-items-center">
-        <div class="d-flex flex-row  align-items-center">
-          <img src="https://avatars.githubusercontent.com/u/17029609?v=4" width="50" height="50" class="rounded-circle">
-          <div class="name-member p-2">kratinho Lima</div>
-          <div><span class="badge text-bg-secondary">Admin</span></div>
-        </div>
-      </div>
-      <div class="description-member">Possuo 12 anos de experiência na área, sou criativo e apaixonado por
-        desenvolvimento web, possuo facilidade para trabalhar em equipe</div>
-      <div class="d-inline-flex category-article py-2  align-items-center">
-        <div class="view-article">Escreveu <strong>8</strong> artigos e teve <strong>3300</strong> visualizações</div>
-      </div>
-    </div>
-  </article>
+    </article>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'CardMember',
+  data() {
+    return {
+      members: [
+        {
+          name: 'Fagner Mendes',
+          image: 'https://avatars.githubusercontent.com/u/34191081?v=4',
+          role: 'Admin',
+          description: 'Possuo 12 anos de experiência na área, sou criativo e apaixonado por desenvolvimento web, possuo facilidade para trabalhar em equipe',
+          articles: 8,
+          views: 3300
+        },
+        {
+          name: 'kratinho Lima',
+          image: 'https://avatars.githubusercontent.com/u/17029609?v=4',
+          role: 'Admin',
+          description: 'Possuo 12 anos de experiência na área, sou criativo e apaixonado por desenvolvimento web, possuo facilidade para trabalhar em equipe',
+          articles: 8,
+          views: 3300
+        },
+        {
+          name: 'kratinho Lima',
+          image: 'https://avatars.githubusercontent.com/u/17029609?v=4',
+          role: 'Admin',
+          description: 'Possuo 12 anos de experiência na área, sou criativo e apaixonado por desenvolvimento web, possuo facilidade para trabalhar em equipe',
+          articles: 8,
+          views: 3300
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 article {
@@ -61,9 +83,3 @@ article {
   color: #757575;
 }
 </style>
-<script>
-
-export default {
-  name: 'CardMember',
-}
-</script>
