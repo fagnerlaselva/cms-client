769388398d546fefbf2e2e0c13b77fba377ee119
Fagner La Selva <fagnerlaselva@gmail.com>
2024-05-16 08:52:51 -0300

    add: component article form

--- src/views/ArticleEditor.vue
+++ src/views/ArticleEditor.vue
@@ -25,64 +25,7 @@
         </div>
       </div>
 
-      <div class="container py-6">
-        <form class="py-6">
-          <ImageUploader labelText="Selecione uma imagem" inputId="image-perfil" />
-          <div class="row my-3">
-            <label for="colFormLabelName" class="col-sm-3 col-form-label text-md-end">Título do artigo:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control custom-file-input-label" id="colFormLabelName" placeholder=""
-                value="Desvendando Estratégias: Identificação e Ampliação de Vendas para Expansão Empresarial">
-            </div>
-          </div>
-
-          <div class="row my-4">
-            <label for="colFormLabelSurname" class="col-sm-3 col-form-label text-md-end">Titulo da página:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="colFormLabelSurname" placeholder=""
-                value="Técnicas para Identificar e Aumentar Vendas e Expandir Negócios">
-              <span class="text-muted">
-                *As melhores title tags têm até, no máximo, 60 caracteres, para não extrapolar o limite permitido para
-                visualização no Google, Bing, etc.
-              </span>
-            </div>
-          </div>
-
-          <div class="row mb-4">
-            <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label text-md-end">Descrição
-              página:</label>
-            <div class="col-sm-8">
-              <textarea v-model="message" class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
-              <small class="text-muted">*Uma meta description deve ter, no máximo, 160 caracteres. Você colocou
-                <strong>{{
-              message.length }}</strong> caracteres</small>
-            </div>
-          </div>
-
-          <div class="row my-4">
-            <label for="colFormLabelSurname" class="col-sm-3 col-form-label text-md-end">Slug URL:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="colFormLabelSurname" placeholder="" value="blog/mailing-list">
-              <span class="text-muted">
-                *Uma URL amigável para SEO é uma URL simples e compreensível que contém palavras-chave relevantes e
-                descreve claramente o conteúdo da página.
-              </span>
-            </div>
-          </div>
-
-          <div class="row my-4">
-            <label for="colFormLabelSurname" class="col-sm-3 col-form-label text-md-end">Canonicol URL:</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="colFormLabelSurname" placeholder="" value="blog/mailing-list">
-              <span class="text-muted">
-                *O Link Canonical é uma tag que define a URL preferida de uma página, evitando conteúdo duplicado e
-                ajudando na classificação nos mecanismos de busca.
-              </span>
-            </div>
-          </div>
-
-        </form>
-      </div>
+      <ArticleForm></ArticleForm>
     </div>
     <SidebarArticle></SidebarArticle>
   </section>
@@ -142,18 +85,13 @@ form {
 }
 </style>
 <script>
-import ImageUploader from '@/components/generic/forms/UploadBanner.vue';
+import ArticleForm from "@/components/app/articleForm/ArticleForm.vue"
 import SidebarArticle from "@/components/generic/SidebarArticle.vue"
 export default {
-  components: { ImageUploader, SidebarArticle },
+  components: { SidebarArticle, ArticleForm },
   name: 'ArticleSeo',
   data() {
     return {
-      imagePreviewUrl: null,
-      imageLoaded: false,
-      labelText: "Selecione uma imagem",
-      backgroundImageStyle: null,
-      message: 'Potencialize vendas e expanda seu negócio com a Contatus. Identificamos potenciais clientes através de perfis detalhados e estatísticas para segmentação.',
       SeoPalavrasIdeial: 700,
     };
   },
