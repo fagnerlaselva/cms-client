47874dfe0d6962e521a87b45ea7ea84c5cd8430a
Fagner La Selva <fagnerlaselva@gmail.com>
2024-07-09 20:23:11 -0300

    add: component Chart

---
+++ src/components/app/charts/BarChart.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="card border-none p-sm-2 mt-2 mb-4">
+    <div class="col-12 col-sm-12 col-lg-12 p-sm-4 top-list">
+      <div>
+        <canvas height="240px" id="acquisitions"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+
+export default {
+  name: 'LineChart',
+  setup() {
+    onMounted(() => {
+      const data = [
+        { day: 1, count: 10 },
+        { day: 2, count: 20 },
+        { day: 3, count: 15 },
+        { day: 4, count: 25 },
+        { day: 5, count: 22 },
+        { day: 6, count: 30 },
+        { day: 7, count: 28 },
+        { day: 8, count: 17 },
+        { day: 9, count: 24 },
+        { day: 10, count: 21 },
+        { day: 11, count: 103 },
+        { day: 12, count: 27 },
+        { day: 13, count: 19 },
+        { day: 14, count: 29 },
+        { day: 15, count: 50 },
+        { day: 16, count: 100 },
+        { day: 17, count: 101 },
+        { day: 18, count: 90 },
+        { day: 19, count: 103 },
+        { day: 20, count: 103 },
+        { day: 21, count: 50 },
+        { day: 22, count: 22 },
+        { day: 23, count: 30 },
+        { day: 24, count: 28 },
+        { day: 25, count: 17 },
+        { day: 26, count: 24 },
+        { day: 27, count: 21 },
+        { day: 28, count: 13 },
+        { day: 29, count: 27 },
+        { day: 30, count: 19 },
+        { day: 31, count: 29 }
+      ];
+
+      new Chart(
+        document.getElementById('acquisitions'),
+        {
+          type: 'line',
+          data: {
+            labels: data.map(row => row.day),
+            datasets: [
+              {
+                pointRadius: 0,
+                label: '  Visualizações ',
+                data: data.map(row => row.count),
+                backgroundColor: 'rgba(241, 99, 52, 0.3)',
+                borderColor: '#f16334',
+                borderWidth: 3,
+                fill: true,
+                tension: 0.3, // This makes the lines curved. Set to 0 for straight lines.
+                pointBackgroundColor: '#f16334',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: '#fff'
+              }
+            ]
+          },
+          options: {
+            interaction: {
+              intersect: false,
+              mode: 'index',
+            },
+            responsive: true,
+            scales: {
+              x: {
+                display: true, // Hide the x-axis
+                ticks: {
+                  display: true
+                },
+                border: {
+                  dash: [5, 10],
+                  color: 'rgba(211, 69, 52, 0.7)'
+                },
+                grid: {
+                  drawTicks: true,
+                  display: true, // Hide the x-axis grid lines
+                  drawBorder: false,
+                  borderDash: [5, 5],
+                  color: 'rgba(211, 69, 52, 0.2)' // Set x-axis border color to white (if displayed)
+                }
+              },
+              y: {
+                display: false,
+                ticks: {
+                  display: false
+                },
+                grid: {
+                  display: false
+                },
+                border: {
+                  dash: [5, 10],
+                  color: 'rgba(211, 69, 52, 0.2)' // Set y-axis border color to white (if displayed)
+                },
+                suggestedMin: 0,
+                suggestedMax: 100
+              }
+            },
+            plugins: {
+              legend: {
+                display: true,
+                position: 'top',
+                align: 'end',
+                borderRadius: 10,
+                labels: {
+                  pointStyle: 'circle',
+                  usePointStyle: true,
+                  borderRadius: 10,
+                }
+              },
+              tooltip: {
+                backgroundColor: '#101214',
+                titleColor: '#fff',
+                bodyColor: '#fff',
+                borderWidth: 20,
+                borderRadius: 20,
+                titleFont: { weight: 'bold' },
+                padding: 20, // Added padding to tooltip
+                callbacks: {
+                  title: function (context) {
+                    const date = new Date(2024, 7, context[0].dataIndex + 1);
+                    return date.toLocaleDateString('pt-BR', { day: 'numeric', month: 'short', year: 'numeric' });
+                  },
+                  label: function (context) {
+                    let label = context.dataset.label || '';
+                    if (label) {
+                      label += ': ';
+                    }
+                    label += context.raw.toLocaleString();
+                    return label + '  Leitores';
+                  },
+                  labelPointStyle: function () {
+                    return {
+                      borderRadius: 20,
+                      pointStyle: 'circle',
+                      rotation: 0,
+                      backgroundColor: '#f16334',
+                    };
+                  }
+                },
+                displayColors: true
+              }
+            }
+          }
+        }
+      );
+    });
+  }
+};
+</script>
+
+<style scoped>
+canvas {
+  /* Adjusted height */
+  width: 100%;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -34,10 +34,11 @@
           <div class="collapse navbar-collapse" id="navbarNavDarkDropdown" style="display: block;">
             <ul class="navbar-nav">
               <li class="nav-item dropdown">
-                <button class="btn dropdown-toggle float-end" data-bs-toggle="dropdown" aria-expanded="false">
-                  Hoje
+                <button class="btn dropdown-toggle float-end" data-bs-toggle="dropdown" aria-expanded="true">
+                  Mês atual
                 </button>
                 <ul class="dropdown-menu dropdown-menu ">
+                  <li><a class="dropdown-item" href="#">Hoje</a></li>
                   <li><a class="dropdown-item" href="#">Ontem</a></li>
                   <li><a class="dropdown-item" href="#">Mês atual</a></li>
                 </ul>
@@ -51,7 +52,9 @@
         <AddButton :link="{ name: 'AddCategory' }" buttonText="Categoria" />
         <AddButton :link="{ name: 'AddAuthor' }" buttonText="Autor" />
       </div>
+
       <ReportCard :cardData="cardData" />
+      <BarChart />
       <div class="g-4 rounded-4">
         <div class="row">
           <div class="col-12 col-sm-12 col-lg-6 pb-4 top-list">
@@ -117,50 +120,11 @@
                   </div>
                 </div>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/marketing-direto-notbook.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Marketing Direto</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    10000 Views
-                  </div>
-                </div>
-              </li>
             </ol>
           </div>
           <div class="col-12 col-sm-12 col-lg-6 pb-4 top-list">
             <ol class="p-0 p-sm-3 list-group list-group-numbered rounded-4">
               <div class="h4 text-primary-emphasis pb-2 px-3 mt-4">Top Artigos</div>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/marketing-direto-notbook.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Organizando sua empresa e implementando as...
-                    </div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    5000 Views
-                  </div>
-                </div>
-              </li>
               <li class="list-group-item d-flex justify-content-between align-items-start">
                 <div class="me-auto text-secondary d-flex">
                   <div class="image-container rounded-3"
@@ -287,7 +251,7 @@
         </div>
       </div>
     </div>
-    <div class="marquee">
+    <div class="marquee d-none">
       <div>
         <span>Em breve novidades</span>
         <span>Em breve novidades</span>
@@ -311,10 +275,13 @@
 <script>
 import ReportCard from '@/components/app/card/ReportCard.vue';
 import AddButton from '@/components/generic/triggers/AddButton.vue';
+import BarChart from '@/components/app/charts/BarChart.vue';
+
+BarChart
 export default {
   name: 'ViewReport',
   components: {
-    ReportCard, AddButton
+    ReportCard, AddButton, BarChart
   },
   data() {
     return {
