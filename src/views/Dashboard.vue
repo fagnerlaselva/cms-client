7ab9ba0cd620826ceb27c63e47bcadd0eab72c85
Fagner La Selva <fagnerlaselva@gmail.com>
2024-07-09 21:11:41 -0300

    route manifest

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -57,195 +57,14 @@
       <BarChart />
       <div class="g-4 rounded-4">
         <div class="row">
-          <div class="col-12 col-sm-12 col-lg-6 pb-4 top-list">
-            <ol class="p-0 p-sm-3 list-group list-group-numbered rounded-4">
-              <div class="h4 text-primary-emphasis pb-2 px-3 mt-4 d-flex flex-row justify-content-between">
-                <div>Top categorias</div>
-                <div></div>
-              </div>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/pog/melhores-lista-de-contatos-telemarketing.jpg');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Mailing</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    15000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/mailing-de-clientes-9-dicas.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Mailing List</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    12000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/identificar-e-aumentar-vendas-expandir-negocios.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Marketing Direto</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    10000 Views
-                  </div>
-                </div>
-              </li>
-            </ol>
-          </div>
-          <div class="col-12 col-sm-12 col-lg-6 pb-4 top-list">
-            <ol class="p-0 p-sm-3 list-group list-group-numbered rounded-4">
-              <div class="h4 text-primary-emphasis pb-2 px-3 mt-4">Top Artigos</div>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/marketing-direto-notbook.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Organizando sua empresa e implementando as...
-                    </div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    5000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/marketing-direto-notbook.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Organizando sua empresa e implementando as...
-                    </div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    5000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://contatus.net.br/blog-image/marketing-direto-notbook.webp');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Organizando sua empresa e implementando as...
-                    </div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    5000 Views
-                  </div>
-                </div>
-              </li>
-            </ol>
-          </div>
+
+          <TopCategories :categories="TopCategories" />
+          <TopArticles :articles="topArticles" />
           <div class="col-12 pb-4 top-list">
             <ol class="p-0 p-sm-3 list-group rounded-4 list-group-horizontal-lg">
               <div class="h4 text-primary-emphasis pb-2 px-3 mt-4 col-12 col-lg-2">Top Autores</div>
-              <li class="list-group-item col-12 col-sm-3">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3" style="background-image: url('/images/foto-marca-cms.jpg');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Lu Maia</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    10000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item col-12 col-sm-3">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://avatars.githubusercontent.com/u/34191081?v=4');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Fafa Mendes</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    10000 Views
-                  </div>
-                </div>
-              </li>
-              <li class="list-group-item col-12 col-sm-3">
-                <div class="me-auto text-secondary d-flex">
-                  <div class="image-container rounded-3"
-                    style="background-image: url('https://avatars.githubusercontent.com/u/17029609?v=4');">
-                  </div>
-                  <div class="px-2">
-                    <div class="text-primary-emphasis fw-medium">Kratinho Lima</div>
-                    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M2.42012 12.7132C2.28394 12.4975 2.21584 12.3897 2.17772 12.2234C2.14909 12.0985 2.14909 11.9015 2.17772 11.7766C2.21584 11.6103 2.28394 11.5025 2.42012 11.2868C3.54553 9.50484 6.8954 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7766C21.8517 11.9015 21.8517 12.0985 21.8231 12.2234C21.785 12.3897 21.7169 12.4975 21.5807 12.7132C20.4553 14.4952 17.1054 19 12.0004 19C6.8954 19 3.54553 14.4952 2.42012 12.7132Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path
-                        d="M12.0004 15C13.6573 15 15.0004 13.6569 15.0004 12C15.0004 10.3431 13.6573 9 12.0004 9C10.3435 9 9.0004 10.3431 9.0004 12C9.0004 13.6569 10.3435 15 12.0004 15Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    10000 Views
-                  </div>
-                </div>
-              </li>
+              <TopAutor v-for="author in topAuthors" :key="author.id" :image-url="author.image"
+                :author-name="author.name" :views="author.views" />
             </ol>
           </div>
         </div>
@@ -274,14 +93,19 @@
 
 <script>
 import ReportCard from '@/components/app/card/ReportCard.vue';
+import TopArticles from '@/components/app/card/TopArticles.vue';
+import TopAutor from '@/components/app/card/TopAutor.vue';
+import TopCategories from '@/components/app/card/TopCategories.vue';
 import AddButton from '@/components/generic/triggers/AddButton.vue';
 import BarChart from '@/components/app/charts/BarChart.vue';
+
+
 
 BarChart
 export default {
   name: 'ViewReport',
   components: {
-    ReportCard, AddButton, BarChart
+    ReportCard, TopArticles, TopCategories, TopAutor, AddButton, BarChart
   },
   data() {
     return {
@@ -292,6 +116,66 @@ export default {
         { label: 'Categorias', value: 6 },
         { label: 'Rascunhos', value: 2 },
         { label: 'Arquivados', value: 2 },
+      ],
+      topAuthors: [
+        {
+          id: 1,
+          name: 'Lu Maia',
+          image: '/images/foto-marca-cms.jpg',
+          views: 10000
+        },
+        {
+          id: 2,
+          name: 'Fafa Mendes',
+          image: 'https://avatars.githubusercontent.com/u/34191081?v=4',
+          views: 10000
+        },
+        {
+          id: 3,
+          name: 'Kratinho Lima',
+          image: 'https://avatars.githubusercontent.com/u/17029609?v=4',
+          views: 10000
+        }
+      ],
+      topArticles: [
+        {
+          id: 1,
+          title: 'Organizando sua empresa e implementando as...',
+          image: 'https://contatus.net.br/blog-image/marketing-direto-notbook.webp',
+          views: 5000
+        },
+        {
+          id: 2,
+          title: 'Organizando sua empresa e implementando as...',
+          image: 'https://contatus.net.br/blog-image/marketing-direto-notbook.webp',
+          views: 5000
+        },
+        {
+          id: 3,
+          title: 'Organizando sua empresa e implementando as...',
+          image: 'https://contatus.net.br/blog-image/marketing-direto-notbook.webp',
+          views: 5000
+        }
+      ],
+      TopCategories: [
+        {
+          id: 1,
+          name: 'Mailing',
+          image: 'https://contatus.net.br/blog-image/pog/melhores-lista-de-contatos-telemarketing.jpg',
+          views: 15000
+        },
+        {
+          id: 2,
+          name: 'Mailing List',
+          image: 'https://contatus.net.br/blog-image/mailing-de-clientes-9-dicas.webp',
+          views: 12000
+        },
+        {
+          id: 3,
+          name: 'Marketing Direto',
+          image: 'https://contatus.net.br/blog-image/identificar-e-aumentar-vendas-expandir-negocios.webp',
+          views: 10000
+        }
       ]
     };
   }
