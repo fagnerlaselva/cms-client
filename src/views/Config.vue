9fca01aeca057f39c377f8e596917c4415175ead
Fagner La Selva <fagnerlaselva@gmail.com>
2024-08-05 10:22:53 -0300

    clear

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -22,37 +22,21 @@
         <div class="card mb-3 rounded-4">
           <div class="card-body text-secondary">
             <div class="text-secondary rounded-4">
-              <RouterLink :to="{ name: 'SeoBlog' }"
+              <RouterLink :to="{ name: 'Profile' }"
                 class="dropdown-item text-primary-emphasis fs-6 py-2 border-none d-flex row align-items-center">
                 <div class="col-2 col-sm-1 justify-content-center">
                   <div class="mx-auto" style="width: 35px">
-                    <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M14 11H8M10 15H8M16 7H8M20 10.5V6.8C20 5.11984 20 4.27976 19.673 3.63803C19.3854 3.07354 18.9265 2.6146 18.362 2.32698C17.7202 2 16.8802 2 15.2 2H8.8C7.11984 2 6.27976 2 5.63803 2.32698C5.07354 2.6146 4.6146 3.07354 4.32698 3.63803C4 4.27976 4 5.11984 4 6.8V17.2C4 18.8802 4 19.7202 4.32698 20.362C4.6146 20.9265 5.07354 21.3854 5.63803 21.673C6.27976 22 7.11984 22 8.8 22H11.5M22 22L20.5 20.5M21.5 18C21.5 19.933 19.933 21.5 18 21.5C16.067 21.5 14.5 19.933 14.5 18C14.5 16.067 16.067 14.5 18 14.5C19.933 14.5 21.5 16.067 21.5 18Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <img src="https://avatars.githubusercontent.com/u/34191081?v=4" alt="Fagner Mendes" width="40"
+                      height="40" class="rounded-4">
                   </div>
                 </div>
                 <div class="col-10 col-sm-11">
-                  <div class="fw-medium"> SEO Bucket</div>
-                  <span class="text-secondary">Informações de SEO</span>
-                </div>
-              </RouterLink>
-
-              <RouterLink :to="{ name: 'SeoBlog' }"
-                class="dropdown-item text-primary-emphasis fs-6 py-2 border-none d-flex row align-items-center d-none">
-                <div class="col-2 col-sm-1">
-                  <div class="mx-auto" style="width: 35px">
-                    <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M17.5 6.5L6.5 6.5M7.8 3H16.2C17.8802 3 18.7202 3 19.362 3.32698C19.9265 3.6146 20.3854 4.07354 20.673 4.63803C21 5.27976 21 6.11984 21 7.8V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V7.8C3 6.11984 3 5.27976 3.32698 4.63803C3.6146 4.07354 4.07354 3.6146 4.63803 3.32698C5.27976 3 6.11984 3 7.8 3Z"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                  <div class="fw-medium">
+                    {{ user.name }}
                   </div>
-                </div>
-                <div class="col-10">
-                  <div class="fw-medium"> Layout </div>
-                  <span class="text-secondary">Personalizar seu Bucket</span>
+                  <span class="text-secondary">
+                    {{ user.email }}
+                  </span>
                 </div>
               </RouterLink>
             </div>
@@ -80,6 +64,46 @@
           </div>
         </div>
       </div>
+      <div class="card mb-3 rounded-4">
+        <div class="card-body text-secondary">
+          <div class="text-secondary rounded-4">
+            <RouterLink :to="{ name: 'SeoBlog' }"
+              class="dropdown-item text-primary-emphasis fs-6 py-2 border-none d-flex row align-items-center">
+              <div class="col-2 col-sm-1 justify-content-center">
+                <div class="mx-auto" style="width: 35px">
+                  <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M14 11H8M10 15H8M16 7H8M20 10.5V6.8C20 5.11984 20 4.27976 19.673 3.63803C19.3854 3.07354 18.9265 2.6146 18.362 2.32698C17.7202 2 16.8802 2 15.2 2H8.8C7.11984 2 6.27976 2 5.63803 2.32698C5.07354 2.6146 4.6146 3.07354 4.32698 3.63803C4 4.27976 4 5.11984 4 6.8V17.2C4 18.8802 4 19.7202 4.32698 20.362C4.6146 20.9265 5.07354 21.3854 5.63803 21.673C6.27976 22 7.11984 22 8.8 22H11.5M22 22L20.5 20.5M21.5 18C21.5 19.933 19.933 21.5 18 21.5C16.067 21.5 14.5 19.933 14.5 18C14.5 16.067 16.067 14.5 18 14.5C19.933 14.5 21.5 16.067 21.5 18Z"
+                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </div>
+              </div>
+              <div class="col-10 col-sm-11">
+                <div class="fw-medium"> SEO Bucket</div>
+                <span class="text-secondary">Informações de SEO</span>
+              </div>
+            </RouterLink>
+            <RouterLink :to="{ name: 'SeoBlog' }"
+              class="dropdown-item text-primary-emphasis fs-6 py-2 border-none d-flex row align-items-center d-none">
+              <div class="col-2 col-sm-1">
+                <div class="mx-auto" style="width: 35px">
+                  <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M17.5 6.5L6.5 6.5M7.8 3H16.2C17.8802 3 18.7202 3 19.362 3.32698C19.9265 3.6146 20.3854 4.07354 20.673 4.63803C21 5.27976 21 6.11984 21 7.8V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V7.8C3 6.11984 3 5.27976 3.32698 4.63803C3.6146 4.07354 4.07354 3.6146 4.63803 3.32698C5.27976 3 6.11984 3 7.8 3Z"
+                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </div>
+              </div>
+              <div class="col-10">
+                <div class="fw-medium"> Layout </div>
+                <span class="text-secondary">Personalizar seu Bucket</span>
+              </div>
+            </RouterLink>
+          </div>
+        </div>
+      </div>
+
+
 
       <div class="card mb-3 rounded-4">
         <div class="card-body text-secondary">
@@ -131,6 +155,11 @@ export default {
   name: 'ViewConfig',
   components: {
     ThemeSwitcher,
+  },
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem('userData'))
+    }
   },
   methods: {
     logout() {
