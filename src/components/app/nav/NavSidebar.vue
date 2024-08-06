fc906c25c3cd5d85608b22a13bc44a12ddbb90b1
Fagner La Selva <fagnerlaselva@gmail.com>
2024-08-06 10:04:03 -0300

    add: modal Glassmorphism

--- src/components/app/nav/NavSidebar.vue
+++ src/components/app/nav/NavSidebar.vue
@@ -79,7 +79,6 @@
                 </li>
             </ul>
             <div class="dropdown">
-
                 <a href="#" class="d-flex align-items-center justify-content-center p-3 align-items-center"
                     data-bs-toggle="dropdown" aria-expanded="true">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
