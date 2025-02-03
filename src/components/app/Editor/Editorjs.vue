0fa461b17655a73bbf97eebaf24996ad22d96b27
Fagner La Selva <fagnerlaselva@gmail.com>
2025-02-03 15:29:18 -0300

    fix: Super modal's  Kratinho 2

--- src/components/app/Editor/Editorjs.vue
+++ src/components/app/Editor/Editorjs.vue
@@ -286,7 +286,7 @@ export default {
 
       const modalEl = this.$refs.modalUpdateArticle;
       const modal = Modal.getInstance(modalEl);
-      modal.hide();
+      if (modal) modal.hide();
 
 
       const output = await this.editor.save();
