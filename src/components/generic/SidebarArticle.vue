2d39bb984a7fdbad152c58e283c7c2a820c3d015
fagnerlaselva <fagnerlaselva@gmail.com>
2025-10-03 10:54:50 -0300

    chore: toast-header

--- src/components/generic/SidebarArticle.vue
+++ src/components/generic/SidebarArticle.vue
@@ -114,16 +114,16 @@
         </transition>
 
         <!-- Toast -->
-        <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div class="toast-container position-fixed bottom-0 end-0 p-3 border-0">
             <div ref="liveToast" id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="toast-header">
                     <!-- <img src="..." class="rounded me-2" alt="..."> -->
-                    <strong class="me-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
+                    <strong class="me-auto">🚀 Uhuuuuu!!!!!</strong>
+                    <small>Agora</small>
                     <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
                 <div class="toast-body">
-                    Hello, world! This is a toast message.
+                    Salvo com sucesso!
                 </div>
             </div>
         </div>
